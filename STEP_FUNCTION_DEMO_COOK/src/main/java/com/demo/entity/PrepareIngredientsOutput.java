db2827b573a5b33655a48d47508f827da149b5fc
uopsdod <uopsdod@gmail.com>
2018-10-20 04:08:09 +0800

    PrepareIngredients change result value add PrepareIngredientsOutput entity add PrepareIngredientsInput entity

---
+++ STEP_FUNCTION_DEMO_COOK/src/main/java/com/demo/entity/PrepareIngredientsOutput.java
@@ -0,0 +1,24 @@
+package com.demo.entity;
+
+public class PrepareIngredientsOutput {
+	String ingredients;
+	int number;
+	public PrepareIngredientsOutput(String ingredients, int number) {
+		super();
+		this.ingredients = ingredients;
+		this.number = number;
+	}
+	public String getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(String ingredients) {
+		this.ingredients = ingredients;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+}
--- STEP_FUNCTION_DEMO_COOK/src/main/java/com/demo/lambda/PrepareIngredients.java
+++ STEP_FUNCTION_DEMO_COOK/src/main/java/com/demo/lambda/PrepareIngredients.java
@@ -2,18 +2,22 @@ package com.demo.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.demo.entity.PrepareIngredientsInput;
+import com.demo.entity.PrepareIngredientsOutput;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
-public class PrepareIngredients implements RequestHandler<Object, String> {
+public class PrepareIngredients implements RequestHandler<PrepareIngredientsInput, PrepareIngredientsOutput> {
 
     public PrepareIngredients() {}
 
-    private String getResult() {
-    	return "cilantro,lamb,wine,black pepper";
+    private PrepareIngredientsOutput getResult() {
+    	return new PrepareIngredientsOutput("cilantro,lamb,wine,black pepper", 100);
     }
 
-	public String handleRequest(Object event, Context context) {
+	public PrepareIngredientsOutput handleRequest(PrepareIngredientsInput event, Context context) {
       context.getLogger().log("Received event: " + event);
-      String result = getResult();
+      PrepareIngredientsOutput result = getResult();
       return result;
 	}
     
