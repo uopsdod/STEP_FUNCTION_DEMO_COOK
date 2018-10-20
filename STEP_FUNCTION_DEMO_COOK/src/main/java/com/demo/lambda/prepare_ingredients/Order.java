332eef10e0a3ff466978be472c9c6347442bebdb
uopsdod <uopsdod@gmail.com>
2018-10-20 23:38:02 +0800

    Order: add counter

--- STEP_FUNCTION_DEMO_COOK/src/main/java/com/demo/lambda/prepare_ingredients/Order.java
+++ STEP_FUNCTION_DEMO_COOK/src/main/java/com/demo/lambda/prepare_ingredients/Order.java
@@ -1,5 +1,13 @@
 package com.demo.lambda.prepare_ingredients;
 
 public class Order {
-
+	private static int staticCount = 0;
+	private int orderId;
+	public Order() {
+		this.orderId = ++Order.staticCount;
+	}
+	public int getOrderId() {
+		return orderId;
+	}
+	
 }
